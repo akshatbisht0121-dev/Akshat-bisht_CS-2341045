0a5a74bd1f5e72c79aa5b9dd4de08b70f53e409e
AkshatBisht_CS-2341045_IILM GN <akshatbisht0121@gmail.com>
2026-09-02 15:47:54 +0530

    Add files via upload

---
+++ day-20/prob543.py
@@ -0,0 +1,19 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        self.diameter = 0
+        
+        def height(node):
+            if not node:
+                return 0
+            
+            left = height(node.left)
+            right = height(node.right)
+            
+            # update diameter
+            self.diameter = max(self.diameter, left + right)
+            
+            # return height of current node
+            return 1 + max(left, right)
+        
+        height(root)
+        return self.diameter
